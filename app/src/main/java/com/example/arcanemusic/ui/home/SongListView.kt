ff21381507ea09c0146c7502ec09a5dfb2380860
lauret lucas <lucas.lauret@edu.univ-fcomte.fr>
2024-07-18 21:53:07 +0200

    icon + changement card

--- app/src/main/java/com/example/arcanemusic/ui/home/SongListView.kt
+++ app/src/main/java/com/example/arcanemusic/ui/home/SongListView.kt
@@ -1,15 +1,19 @@
 package com.example.arcanemusic.ui.home
 
 import android.util.Log
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -18,7 +22,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.arcanemusic.R
@@ -79,7 +87,15 @@ fun SongCard(
     music: Music, modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier, elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = modifier
+            .clip(RoundedCornerShape(10.dp))
+            .border(
+                width = 2.dp, color = Color.Red, shape = RoundedCornerShape(10.dp)
+            )
+            .width(50.dp)
+            .height(50.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.Black)
     ) {
         Column(
             modifier = Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -88,17 +104,16 @@ fun SongCard(
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
-                    text = music.titleColumn,
-                    style = MaterialTheme.typography.titleLarge,
+                    text = music.titleColumn, style = MaterialTheme.typography.titleMedium.copy(
+                        color = Color.White, fontWeight = FontWeight.Bold, fontSize = 15.sp
+                    )
                 )
                 Spacer(Modifier.weight(1f))
                 Text(
-                    text = music.artistColumn, style = MaterialTheme.typography.titleMedium
+                    text = music.artistColumn,
+                    style = MaterialTheme.typography.titleMedium.copy(color = Color.Gray)
                 )
             }
-            Text(
-                text = music.dataColumn, style = MaterialTheme.typography.titleMedium
-            )
         }
     }
 }
